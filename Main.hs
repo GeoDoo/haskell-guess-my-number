00e3096c04fa404747ca91df557e13276dc0f212
Georgios Karametas <geometas@hotmail.com>
2022-01-20 10:07:56 +0000

    fix string (wrong) comparison to int comparison

--- Main.hs
+++ Main.hs
@@ -1,6 +1,6 @@
 turn :: Int -> String -> IO ()
 turn n guess = do
-  if show n > guess
+  if n > (read guess :: Int)
     then do
       putStrLn "Too low!"
       guessmynumber n
@@ -12,6 +12,6 @@ guessmynumber :: Int -> IO ()
 guessmynumber n = do
   putStr "Enter your guess: "
   guess <- getLine
-  if show n == guess
+  if n == (read guess :: Int)
     then putStrLn ("You guessed right this time! My number was " ++ show n)
     else turn n guess
